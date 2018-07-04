e2fe8a267d3aa0dc983522bab4a6b84d485bae8a
lange-m <ml@michallange.com>
2018-07-04 22:05:42 +0200

    initial

---
+++ src/main/java/pl/com/psipoznan/visitorsregistry/visitorsregistry/VisitorsRegistryApplication.java
@@ -0,0 +1,12 @@
+package pl.com.psipoznan.visitorsregistry.visitorsregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorsRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorsRegistryApplication.class, args);
+	}
+}
